2a106da3ee344c6fd4da6e0f1d00c484f2f03177
NISHANT KUMAR <abc.nishant007@gmail.com>
2023-01-30 01:55:30 +0800

    ENH: allowed plotting test score as well

--- tryouts/timeseries/univariate/toy_examples_classical_ml.py
+++ tryouts/timeseries/univariate/toy_examples_classical_ml.py
@@ -16,9 +16,9 @@ DEBUG = False
 
 
 def generate_sine_curve_time_series(noise: bool):
-    time_sampler = ts.TimeSampler(stop_time=20)
-    irregular_time_samples = time_sampler.sample_irregular_time(num_points=50000, keep_percentage=50)
-    sinusoid = ts.signals.Sinusoidal(frequency=0.25)
+    time_sampler = ts.TimeSampler(stop_time=30)
+    irregular_time_samples = time_sampler.sample_irregular_time(num_points=5000, keep_percentage=50)
+    sinusoid = ts.signals.Sinusoidal(frequency=0.5)
 
     white_noise = ts.noise.GaussianNoise(std=0.3)
 
@@ -65,16 +65,18 @@ def time_series_to_supervised(x: list or np.array, input_seq_len, output_seq_len
         return_dict = {}
         return_dict["X_train"] = x[:n1, :]
         return_dict["Y_train"] = y[:n1, :]
-        return_dict["X_val"] = y[n1 : n1 + n2, :]
+        return_dict["X_val"] = x[n1 : n1 + n2, :]
         return_dict["Y_val"] = y[n1 : n1 + n2, :]
         return_dict["X_test"] = y[n1 + n2 :, :]
         return_dict["Y_test"] = y[n1 + n2 :, :]
         return return_dict
 
     data_splits = []
+    blocksize = X.shape[0] // n_blocks
     for i in range(n_blocks):
-        x = X[i * X.shape[0] // n_blocks : (i + 1) * X.shape[0] // n_blocks, :]
-        y = Y[i * X.shape[0] // n_blocks : (i + 1) * X.shape[0] // n_blocks, :]
+        x = X[i * blocksize : (i + 1) * blocksize, :]
+        y = Y[i * blocksize : (i + 1) * blocksize, :]
+        print(x.shape, y.shape)
         data_splits.append(split_a_single_block(x, y, train_test_validation_ratio=tvt_ratio))
 
     data_dict_combined = {}
@@ -90,6 +92,9 @@ def time_series_to_supervised(x: list or np.array, input_seq_len, output_seq_len
 
 
 def train_and_predict_RF(data_splits, data_dict_combined, max_depth=30, n_estimators=100):
+    """
+    :return: mape, mse, mape_test, mse_test, model_list, fitted_model_list
+    """
     fitted_model_list = []
     for i in range(len(data_splits)):
         data = data_splits[i]
@@ -106,11 +111,33 @@ def train_and_predict_RF(data_splits, data_dict_combined, max_depth=30, n_estima
 
     mape = []
     mse = []
-    for model in fitted_model_list:
+    mse_test = []
+    mape_test = []
+    plt.clf()
+    for count, model in enumerate(fitted_model_list):
         y_pred = model.predict(data_dict_combined["X_val"])
-        mape.append(sklearn.metrics.mean_absolute_error(y_pred, data_dict_combined["Y_val"]))
+        mape.append(sklearn.metrics.mean_absolute_percentage_error(y_pred, data_dict_combined["Y_val"]))
         mse.append(sklearn.metrics.mean_absolute_error(y_pred, data_dict_combined["Y_val"]))
-    return mape, mse, fitted_model_list
+
+        y_pred = model.predict(data_dict_combined["X_val"])
+        mape_test.append(sklearn.metrics.mean_absolute_percentage_error(y_pred, data_dict_combined["Y_val"]))
+        mse_test.append(sklearn.metrics.mean_absolute_error(y_pred, data_dict_combined["Y_val"]))
+        if DEBUG:
+            if count == 0:
+                for i in tqdm(range(y_pred.shape[0]), desc="Plotting"):
+                    if np.random.rand() <= 1:
+                        plt.plot(range(i, i + y_pred.shape[1]), y_pred[i, :], label="pred", color="red", alpha=0.4)
+                        plt.plot(
+                            range(i, i + y_pred.shape[1]),
+                            data_dict_combined["Y_val"][i, :],
+                            label="GT",
+                            color="blue",
+                            alpha=0.4,
+                        )
+    # rr = int(np.random.rand() * (y_pred.shape[0]))
+    # plt.xlim(rr, rr+24)
+    plt.show()
+    return mape, mse, mape_test, mse_test, fitted_model_list
 
     # y_multirf = regr_multirf.predict(X_test)
     # y_rf = regr_multirf.predict(X_test)
@@ -129,12 +156,28 @@ def plot_random_100_predictions(data):
     plt.show()
 
 
+def read_real_data(sensor_id, cutoff_for_plotting=1200):
+    assert sensor_id <= 962
+    count = 0
+    a = []
+    with open("PEMS_train") as f:
+        for row in f:
+            a = a + eval(
+                "[" + str(row.split(";")[sensor_id].split(" ")).replace("[", "").replace("]", "").replace("'", "") + "]"
+            )
+        plt.plot(a[:cutoff_for_plotting], alpha=0.5)
+        plt.show()
+    return a
+
+
 if __name__ == "__main__":
 
     starttime = time.time()
-    samples, signals, errors = generate_sine_curve_time_series(noise=False)
+    # samples, signals, errors = generate_sine_curve_time_series(noise=False)
     # plot_signals([samples, signals, errors], ["samples", "signals", "errors"])
     # plot_signals([samples, signals, errors], ["samples", "signals", "errors"], plot_or_scatter="scatter")
+
+    samples = read_real_data(sensor_id=1, cutoff_for_plotting=1200)
 
     with open("results.csv", "w") as f:
         csvwriter = csv.writer(f)
@@ -148,34 +191,45 @@ if __name__ == "__main__":
                 "np.std(mape)",
                 "np.mean(mse)",
                 "np.std(mse)",
+                "np.mean(mape_test)",
+                "np.std(mape_test)",
+                "np.mean(mse_test)",
+                "np.std(mse_test)",
             ]
         )
-        for input_seq_len in tqdm([10, 20, 30, 40, 50, 60, 80, 120, 240], desc="input_seq", position=1):
-            output_seq_len = input_seq_len
-            for n_estimators in tqdm([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], desc="n_estimators", position=2):
-                for max_depth in tqdm([5, 10, 20, 30, 40], desc="max_depth", position=3):
+        input_seq_list = [12]
+        n_estimators_list = list(range(2, 100))  # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        max_depth_list = [2, 3, 4, 5, 10, 20, 30]
+        for input_seq_len in tqdm(input_seq_list, desc="input_seq", position=1):
+            output_seq_len = 3
+            for n_estimators in tqdm(n_estimators_list, desc="n_estimators", position=2):
+                for max_depth in tqdm(max_depth_list, desc="max_depth", position=3):
                     data_dict_combined, data_splits = time_series_to_supervised(
                         samples, input_seq_len=input_seq_len, output_seq_len=output_seq_len
                     )
-                    mape, mse, model_list = train_and_predict_RF(
+                    mape, mse, mape_test, mse_test, model_list = train_and_predict_RF(
                         data_splits=data_splits,
                         data_dict_combined=data_dict_combined,
                         n_estimators=n_estimators,
                         max_depth=max_depth,
                     )
 
-                    print(
-                        [
-                            input_seq_len,
-                            output_seq_len,
-                            n_estimators,
-                            max_depth,
-                            np.mean(mape),
-                            np.std(mape),
-                            np.mean(mse),
-                            np.std(mse),
-                        ]
-                    )
+                    # print(
+                    #     [
+                    #         input_seq_len,
+                    #         output_seq_len,
+                    #         n_estimators,
+                    #         max_depth,
+                    #         np.mean(mape),
+                    #         np.std(mape),
+                    #         np.mean(mse),
+                    #         np.std(mse),
+                    #         np.mean(mape_test),
+                    #         np.std(mape_test),
+                    #         np.mean(mse_test),
+                    #         np.std(mse_test),
+                    #     ]
+                    # )
                     csvwriter.writerow(
                         [
                             input_seq_len,
@@ -186,6 +240,10 @@ if __name__ == "__main__":
                             np.std(mape),
                             np.mean(mse),
                             np.std(mse),
+                            np.mean(mape_test),
+                            np.std(mape_test),
+                            np.mean(mse_test),
+                            np.std(mse_test),
                         ]
                     )
                     f.flush()
